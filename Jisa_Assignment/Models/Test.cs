8b4d175603a7c02f7a5cd49b8304b7aa2f1c8386
sagarvk2000 <136054368+sagarvk2000@users.noreply.github.com>
2023-12-14 17:04:07 +0530

    Add project files.

---
+++ Jisa_Assignment/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jisa_Assignment.Models
+{
+    public class Test
+    {
+        //Create a web page with a form that collects user information(name, email, and phone number) and
+        //displays a confirmation message upon submission
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? PhoneNumber { get; set; }
+    }
+}
